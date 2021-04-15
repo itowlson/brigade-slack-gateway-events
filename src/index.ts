1e6e2bbe1071fe968889053af90aeb0abc8dd8e5
itowlson <ivan.towlson@microsoft.com>
2021-04-16 10:11:47 +1200

    Provide client instead of raw responeToken

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,35 @@
 import { Event, events } from '@brigadecore/brigadier';
-import slack from '@slack/bolt';
+import * as slack from '@slack/bolt';
+import * as slackapi from '@slack/web-api';
 
 export class SlackEvents {
-    onSlashCommand(action: (command: slack.SlashCommand, responseToken: string, event: Event) => Promise<void>): void {
+    onSlashCommand(action: (command: slack.SlashCommand, slackClient: slackapi.WebClient, event: Event) => Promise<void>): void {
         events.on('slack', 'slash_command', async (event) => {
             const payload = JSON.parse(event.payload || '');
             const command: slack.SlashCommand = payload.body;
             const responseToken: string = payload.responseToken;
-            await action(command, responseToken, event);
+            const slackClient = new slackapi.WebClient(responseToken);
+            await action(command, slackClient, event);
         });
     }
 
-    onShortcut(action: (shortcut: slack.GlobalShortcut, responseToken: string, event: Event) => Promise<void>): void {
+    onShortcut(action: (shortcut: slack.GlobalShortcut, slackClient: slackapi.WebClient, event: Event) => Promise<void>): void {
         events.on('slack', 'shortcut', async (event) => {
             const payload = JSON.parse(event.payload || '');
             const shortcut: slack.GlobalShortcut = payload.body;
             const responseToken: string = payload.responseToken;
-            await action(shortcut, responseToken, event);
+            const slackClient = new slackapi.WebClient(responseToken);
+            await action(shortcut, slackClient, event);
         });
     }
 
-    onMessageAction(action: (shortcut: slack.MessageShortcut, responseToken: string, event: Event) => Promise<void>): void {
+    onMessageAction(action: (shortcut: slack.MessageShortcut, slackClient: slackapi.WebClient, event: Event) => Promise<void>): void {
         events.on('slack', 'message_action', async (event: Event) => {
             const payload = JSON.parse(event.payload || '');
             const shortcut: slack.MessageShortcut = payload.body;
             const responseToken: string = payload.responseToken;
-            await action(shortcut, responseToken, event);
+            const slackClient = new slackapi.WebClient(responseToken);
+            await action(shortcut, slackClient, event);
         });
     }
 }
